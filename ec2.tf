e5d6348e4a4827673aa6fb46efd7bc345fed7963
Ubuntu <ubuntu@ip-172-31-5-196.ap-south-1.compute.internal>
2026-03-03 22:32:26 +0000

    formatted the ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -23,8 +23,8 @@ resource "aws_instance" "example" {
   }
 
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-  subnet_id = aws_subnet.public_subnet.id
-  lifecycle { 
+  subnet_id              = aws_subnet.public_subnet.id
+  lifecycle {
     ignore_changes = [ami]
   }
 }
